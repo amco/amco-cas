437e9a9edfea67abf868acb764337bcf7e07c11b
Erick Fabian <fabianerickalfonso@gmail.com>
2019-05-28 15:38:23 -0500

    Refactor and add missing required gems

--- amco-cas.gemspec
+++ amco-cas.gemspec
@@ -30,8 +30,8 @@ Gem::Specification.new do |spec|
   spec.add_dependency "rails", ">= 4.2.11.1"
   spec.add_dependency "redis-rails"
   spec.add_dependency "dolly"
+  spec.add_dependency "settingslogic"
 
-  spec.add_development_dependency "sqlite3", "~> 1.3"
   spec.add_development_dependency "bundler", "~> 1.17"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
--- lib/amco/cas/filter.rb
+++ lib/amco/cas/filter.rb
@@ -5,11 +5,13 @@ module Amco
     class Filter < ::RubyCAS::Filter
       class << self
         def filter(controller)
+           raise ArgumentError.new('controller param missing') if controller.nil?
           controller&.params = controller&.cas_params
           super(controller)
         end
 
         def logout(controller, service = nil)
+          raise ArgumentError.new('controller param missing') if controller.nil?
           controller&.params = controller&.cas_params
           super(controller, service)
         end
