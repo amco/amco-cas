3ac68b0c22bbf86c0f68d2040ad41447a1a06aa2
Erick Fabian <fabianerickalfonso@gmail.com>
2019-06-07 12:29:54 -0500

    Enable option to pass settings class name to gem

--- lib/amco/cas.rb
+++ lib/amco/cas.rb
@@ -5,6 +5,9 @@ require 'amco/cas/session'
 
 module Amco
   module Cas
+    mattr_accessor :settings_class_name
+    @@settings_class_name = 'Settings'
+
     class << self
       def configure
         yield config
@@ -12,6 +15,10 @@ module Amco
 
       def config(config = {})
         RubyCAS::Filter.setup(config)
+      end
+
+      def settings_class
+        @@settings_class.constantize
       end
     end
   end
